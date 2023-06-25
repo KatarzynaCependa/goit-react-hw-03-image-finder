92efc1913ff9d9614da9cf226ac209ef091f1aaf
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-06-25 14:57:47 +0200

    Adding task no.2

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import css from 'components/Button/Button.module.css';
+import PropTypes from 'prop-types';
+
+export class Button extends Component {
+  handleClick = e => this.props.onClick(e);
+
+  render() {
+    return (
+      <button type="button" onClick={this.handleClick} className={css.Button}>
+        Load more
+      </button>
+    );
+  }
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
